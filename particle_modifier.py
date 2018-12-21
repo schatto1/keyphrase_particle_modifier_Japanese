53757f62a8229bc3881dabb3c1520d306a866640
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-12-21 12:11:33 -0500

    Adds routine that prints the next three chars Adds routine that identifies the next three chars and prints them to the console

--- particle_modifier.py
+++ particle_modifier.py
@@ -9,11 +9,21 @@ with open(file_address) as current_file:
         data.append(line)
 
     for i in range(len(data)):
-        print("Line String:\n" + data[i])
+        # print("Line String:\n" + data[i])
         document = json.loads(data[i])
+        # Skip first JSON object that's not annotations
         if "maxRead" in document:
             continue
-        print("Raw Text:\n" + document["raw_text"])
+
+        # Iterate through each document in file
+        for j in range(len(document["annotations"]["key_phrase"])):
+            current_text = str(document["raw_text"])
+            current_noun = str(document["annotations"]["key_phrase"][j]["extent"])
+            if current_noun in current_text:
+                print(current_noun + " is in the text")
+                char_number = int(document["annotations"]["key_phrase"][j]["end"])
+                following_three = current_text[char_number] + current_text[char_number+1] + current_text[char_number+2]
+                print(following_three)
         #output = json.dumps(document, indent = 2)
         #print("JSON dump:\n" + output)
 
