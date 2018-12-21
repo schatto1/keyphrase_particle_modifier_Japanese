3b1589e45c6b6da502d942ef0f6902d5ad57877e
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-12-21 14:22:46 -0500

    First iteration of functioning particle_modifier Adds function for appending to JSON file Adds routine to append modified keyphrase documents to outfile

--- particle_modifier.py
+++ particle_modifier.py
@@ -9,34 +9,49 @@ def load_documents(file_address):
     with open(file_address) as current_file:
         for line in current_file:
             data.append(line)
-    return data
+    return data #string
+
+def append_to_file(file_address, document):
+    with open(file_address, "a") as outfile:
+        json.dump(document, outfile)
+        outfile.write("\n")
 
 def check_particles(document, j):
+    #Returns a dictionary
     current_text = str(document["raw_text"])
     text_length = len(current_text)
     current_noun = str(document["annotations"]["key_phrase"][j]["extent"])
     if current_noun in current_text:
-        print(current_noun + " is in the text") #REMOVE LATER
+        # print(current_noun + " is in the text") #REMOVE LATER
         char_number = int(document["annotations"]["key_phrase"][j]["end"])
         if char_number+2 < text_length:
             following_three = current_text[char_number] + current_text[char_number+1] + current_text[char_number+2]
             if following_three in three_char_particles:
-                print(following_three)
-                return
+                #print(following_three) remove later
+                return modify_keyphrase(document, j, current_noun, char_number, following_three, 3)
         if char_number+1 < text_length:
             following_two = current_text[char_number] + current_text[char_number+1]
             if following_two in two_char_particles:
-                print(following_two)
-                return
+                #print(following_two) remove later
+                return modify_keyphrase(document, j, current_noun, char_number, following_two, 2)
         if char_number < text_length:
             following_one = current_text[char_number]
             if following_one in one_char_particles:
-                print(following_one)
-                return
+                #print(following_one) remove later
+                return modify_keyphrase(document, j, current_noun, char_number, following_one, 1)
+    return document #Noun not in text or no particle found; return original dict
+
+def modify_keyphrase(document, j, current_noun, char_number, following_text, number):
+    document["annotations"]["key_phrase"][j]["extent"] = current_noun + following_text
+    document["annotations"]["key_phrase"][j]["end"] = char_number + number
+    return document #dict
+
 
 # MAIN SCRIPT STARTS HERE
 file_address = "./test_SC/complete/Japanese_KP_1120_part1_SC.json"
+output_address = "./test_SC/complete/Japanese_KP_1120_part1_SC_fixed.json"
 data = load_documents(file_address)
+open(output_address, "w").close()
 
 # Iterate through each document in file
 for i in range(len(data)):
@@ -44,10 +59,11 @@ for i in range(len(data)):
 
     # Skip first JSON object that's not annotations
     if "maxRead" in document:
+        append_to_file(output_address, document)
         continue
-
 
     # Iterate through each annotation in document
     key_phrase_length = len(document["annotations"]["key_phrase"])
     for j in range(key_phrase_length):
-        check_particles(document, j)
+        document = check_particles(document, j)
+    append_to_file(output_address, document)
